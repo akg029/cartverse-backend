57dfd46865be4866b6f96448204b6b27633a7171
akg029 <akg2998@gmail.com>
2026-07-03 17:57:40 +0530

    feat: initialize springboot project

---
+++ src/main/java/com/cartverse/cartversebackend/CartverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cartverse.cartversebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartverseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartverse/cartversebackend/CartverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartverse.cartversebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
