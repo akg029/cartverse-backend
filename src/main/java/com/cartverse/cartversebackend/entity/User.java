c26b97b8db5b40bc6d703d35a736a7b2fc299c81
akg029 <akg2998@gmail.com>
2026-07-04 13:02:42 +0530

    feat: adding user Entity

---
+++ src/main/java/com/cartverse/cartversebackend/entity/User.java
@@ -0,0 +1,19 @@
+package com.cartverse.cartversebackend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+}
