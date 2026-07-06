83b864a22c5153ec0eaa2b96494c0efecd8b16b3
akg029 <akg2998@gmail.com>
2026-07-06 17:17:43 +0530

    feat: add Register request dto, AuthController

--- src/main/java/com/cartverse/cartversebackend/service/UserService.java
+++ src/main/java/com/cartverse/cartversebackend/service/UserService.java
@@ -1,7 +1,11 @@
 package com.cartverse.cartversebackend.service;
 
+import com.cartverse.cartversebackend.dto.RegisterRequest;
+import com.cartverse.cartversebackend.entity.User;
 import com.cartverse.cartversebackend.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -11,5 +15,23 @@ public class UserService {
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
+
+    public void registerUser(RegisterRequest request){
+
+        Optional<User> existingUser = userRepository.findByEmail(request.getEmail());
+
+        if (existingUser.isPresent()){
+            throw new RuntimeException("User already Exist with this email");
+        }
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setName(request.getName());
+        user.setPassword(request.getPassword());
+
+        userRepository.save(user);
+    }
+
+
 
 }
