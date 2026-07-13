0f1e205771b611a6e835e9030ca2bdebef200476
akg029 <akg2998@gmail.com>
2026-07-13 13:56:29 +0530

    feat(auth): create JWT authentication filter

--- src/main/java/com/cartverse/cartversebackend/security/JwtAuthenticationFilter.java
+++ src/main/java/com/cartverse/cartversebackend/security/JwtAuthenticationFilter.java
@@ -1,4 +1,27 @@
 package com.cartverse.cartversebackend.security;
 
-public class JwtAuthenticationFilter {
+import com.cartverse.cartversebackend.jwt.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+
+    }
+
+    public JwtAuthenticationFilter(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
 }
